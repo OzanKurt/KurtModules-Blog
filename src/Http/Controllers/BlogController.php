2422e20ecbe239f851ef45e64e99281c05c56461
Ozan Kurt <me@ozankurt.com>
2016-04-30 17:44:27 +0300

    Built in controller updated

--- src/Http/Controllers/BlogController.php
+++ src/Http/Controllers/BlogController.php
@@ -2,10 +2,8 @@
 
 namespace Kurt\Modules\Blog\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+use App\Http\Controllers\Controller;
+
 use Kurt\Modules\Blog\Repositories\Contracts\CategoriesRepositoryInterface;
 use Kurt\Modules\Blog\Repositories\Contracts\PostsRepositoryInterface;
 use Kurt\Modules\Blog\Repositories\Contracts\TagsRepositoryInterface;
@@ -14,29 +12,29 @@ use Kurt\Modules\Blog\Repositories\Contracts\TagsRepositoryInterface;
  * Class BlogController
  * @package Kurt\Modules\Blog\Http\Controllers
  */
-class BlogController extends BaseController
+class BlogController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
     protected $blogCategoriesRepository;
-//    protected $blogCommentsRepository;
     protected $blogPostsRepository;
     protected $blogTagsRepository;
 
     /**
      * Ready up blog repositories.
-     *
-     * @param CategoriesRepositoryInterface $blogCategoriesRepository
-     * @param PostsRepositoryInterface      $blogPostsRepository
-     * @param TagsRepositoryInterface       $blogTagsRepository
      */
-    public function __construct(
-        CategoriesRepositoryInterface $blogCategoriesRepository,
-        PostsRepositoryInterface $blogPostsRepository,
-        TagsRepositoryInterface $blogTagsRepository
-    ) {
-        $this->blogCategoriesRepository = $blogCategoriesRepository;
-        $this->blogPostsRepository = $blogPostsRepository;
-        $this->blogTagsRepository = $blogTagsRepository;
+    public function __construct() {
+        $this->blogCategoriesRepository = app(CategoriesRepositoryInterface::class);
+        $this->blogPostsRepository = app(PostsRepositoryInterface::class);
+        $this->blogTagsRepository = app(TagsRepositoryInterface::class);
+
+        $this->callConstructor();
+    }
+
+    public function callConstructor()
+    {
+        $parent = new \ReflectionClass(parent::class);
+
+        if ($parent->getConstructor()) {
+            parent::__construct();
+        }
     }
 }
